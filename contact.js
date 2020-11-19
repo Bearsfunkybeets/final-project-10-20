704ad62b40b53a780de320c32f0a37f0c36e4546
Sarah Dumont-Rivard <Sarah.dumont@live.com>
2020-11-18 21:35:34 -0600

    project pushing 1st verison.

---
+++ contact.js
@@ -0,0 +1,10 @@
+
+    function validateForm() {
+    if (document.forms[0].myEmail.value == "" ){
+        alert("Please enter a valid email address.");
+    return false;
+    }//end if
+    return true;
+    }// end function validate Form 
+
+
